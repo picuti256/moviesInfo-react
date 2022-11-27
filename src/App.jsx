12459fc1ebca2fc4fd49127532678bd204073672
Giovanne Piçuti <picuti256@gmail.com>
2022-11-27 18:45:59 -0300

    Criação do card de filmes, ajuste do css do navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { Outlet } from 'react-router-dom'
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar/'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <h2>Movies Lib</h2>
       <Outlet />
     </div>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,37 @@
+import { useState, useEffect } from "react";
+import MovieCard from "../components/MovieCard";
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 const home = () => {
+  const [topMovies, setTopMovies] = useState([]);
+
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url)
+    const data = await res.json()
+
+    setTopMovies(data.results)
+    console.log(data.results)
+  };
+
+  useEffect(() => {
+
+    const topRatedURL = `${moviesURL}top_rated?${apiKey}&language=pt-BR
+    `
+
+    getTopRatedMovies(topRatedURL)
+  }, []);
+
   return (
-    <div>home</div>
-  )
-}
+    <div className="container">
+      <h2 className="title">Melhores filmes</h2>
+      <div className="movies-container">
+        {topMovies.push === 0 && <p>Carregando...</p>}
+        {topMovies.length > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie}/>)}
+      </div>
+    </div>
+  );
+};
 
 export default home;
