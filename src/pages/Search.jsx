15f8cf4e07f5c9c6b0ca1b5e5a9965474f6b90a7
Giovanne Piçuti <picuti256@gmail.com>
2022-11-28 09:16:19 -0300

    Ajuste

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -5,7 +5,7 @@ import MovieCard from "../components/MovieCard";
 const searchURL = import.meta.env.VITE_SEARCH
 const apiKey = import.meta.env.VITE_API_KEY 
 
-import './MovieGrid.css';
+import './movieGrid.css';
 
 const search = () => {
   const [searchParams] = useSearchParams();
