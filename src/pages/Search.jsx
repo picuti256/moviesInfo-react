a24359d93b9d958574d14cc36dbc8f2ba1dd43c6
Giovanne Piçuti <picuti256@gmail.com>
2022-11-29 09:12:52 -0300

    Ajuste de .env

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import MovieCard from "../components/MovieCard";
 
-const searchURL = import.meta.env.VITE_SEARCH
-const apiKey = import.meta.env.VITE_API_KEY 
+const searchURL = process.env.VITE_SEARCH
+const apiKey = process.env.VITE_API_KEY 
 
 import './movieGrid.css';
 
